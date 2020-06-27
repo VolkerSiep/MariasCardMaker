3d9e3a12de5325c1af46133865bd73c3e79faecb
Volker Siepmann <volker.siepmann@gmail.com>
2020-06-27 23:23:18 +0200

    add missing version module

---
+++ mcm/version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 27 23:20:57 2020
+
+@author: volker
+"""
+
+version = "V1.0a1"
+release_date = "June 27th, 2020"
+release_year = release_date[-4:]
